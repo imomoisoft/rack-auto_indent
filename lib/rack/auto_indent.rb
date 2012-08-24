218842c6f5410f71bbd3f60e5479ecfe585c5648
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2012-08-24 16:03:09 +0400

    Доработана обработка XML

--- lib/rack/auto_indent.rb
+++ lib/rack/auto_indent.rb
@@ -1,20 +1,27 @@
+require 'nokogiri'
+
 module Rack
   class AutoIndent
     def initialize(app)
-      @app
+      @app = app
     end
 
     def call(env)
       status, headers, response = @app.call(env)
 
-      if headers["Content-Type"].include? "application/xml"
-#        headers.delete('Content-Length')
-        response_body = response.body
-
-        xml = Nokogiri::XML(response.respond_to?(:body) ? response.body : response)
-        p xml.to_s
+      if headers["Content-Type"].include?("application/xml")
+        headers.delete('Content-Length')
+        xml = Nokogiri::XML(response.respond_to?(:body) ? response.body : response) { |x| x.noblanks }
+        formatted_response = Rack::Response.new(
+          xml.to_xml,
+          status,
+          headers
+        )
+        formatted_response.finish
+        formatted_response.to_a
+      else
+        [status, headers, response]
       end
-      [status, headers, formatted_response || response]
     end
   end
 end
